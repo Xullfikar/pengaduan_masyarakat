faab2377177fe13d3831d19335e39c92da01ce77
Andi Zulfikar <andizulfikar831@gmail.com>
2023-02-11 15:32:31 +0700

    first commit

---
+++ src/routes/masyarakat/+layout.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  // @ts-nocheck
+  import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Button
+  } from "sveltestrap";
+
+  let isOpen = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
+
+  import type { LayoutData } from "./$types";
+  export let data: LayoutData;
+</script>
+
+  <Navbar color="primary" dark expand="md">
+    <NavbarBrand href="/">Pengaduan Masyarakat</NavbarBrand>
+    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+      <Nav class="ms-auto" navbar>
+        <NavItem>
+          <NavLink href="/">Dashboard</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/">Pengaduan</NavLink>
+        </NavItem>
+        <Dropdown nav inNavbar>
+          <DropdownToggle nav caret>{}</DropdownToggle>
+          <form method="POST" action="/logout">
+            <DropdownMenu end>
+              <DropdownItem>Nama</DropdownItem>
+              <DropdownItem>NIK</DropdownItem>
+              <DropdownItem>NoTelp</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem type="submit"
+                >Logout</DropdownItem
+              >
+            </DropdownMenu>
+          </form>
+        </Dropdown>
+      </Nav>
+    </Collapse>
+  </Navbar>
+<slot />
